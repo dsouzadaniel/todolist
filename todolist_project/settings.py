d6229cef33bceb0b8d83d1d4199f5be9802446bb
danieldsouza <ddsouza@umich.edu>
2019-05-06 22:00:50 -0400

    Changed up the Style a lil!

--- todolist_project/settings.py
+++ todolist_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'crispy_forms',
+
+    # Local
     'lists.apps.ListsConfig',
 ]
 
@@ -119,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
